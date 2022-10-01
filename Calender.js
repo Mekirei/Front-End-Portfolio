f8e7a3fef4c03dde5844b42be37e13be99fe17ea
Daniel Hill <Danieljhill98@gmail.com>
2022-10-01 06:33:55 -0400

    worked on formatting the calender grid, i will be testing different methods to determine which  day of the week is associated with the day of the month.

--- Calender.js
+++ Calender.js
@@ -1,6 +1,17 @@
 const calender = document.querySelector("#app-calender");
 
-for (let day = 1; day < 32; day++) {
-    calender.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>` );
+const isWeekend = day => {
+    return true;
+}
+
+
+
+
+
+for (let day = 1; day <= 31; day++) {
+
+    const weekend = isWeekend(day);
+
+    calender.insertAdjacentHTML("beforeend", `<div class="day ${weekend ? "weekend" : ""}"  >${day}</div>`);
 }
 
