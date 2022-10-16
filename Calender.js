ce3b49ae1327c1101a5621ccacfeea5f90ef16d7
Daniel Hill <Danieljhill98@gmail.com>
2022-10-16 03:12:56 -0400

    improved functionality of the calender and cleaned up css and layout

--- Calender.js
+++ Calender.js
@@ -1,17 +1,10 @@
 const calender = document.querySelector("#app-calender");
 
-const isWeekend = day => {
-    return true;
-}
 
 
 
 
 
 for (let day = 1; day <= 31; day++) {
-
-    const weekend = isWeekend(day);
-
-    calender.insertAdjacentHTML("beforeend", `<div class="day ${weekend ? "weekend" : ""}"  >${day}</div>`);
-}
-
+    calender.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>`);
+} 
